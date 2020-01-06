8151f0c7d363890273a7171a69a6eb4cf2022b7c
RideSVEl <sbkiking3@gmail.com>
2020-01-06 22:56:03 +0200

    Finish.

---
+++ src/ua/khpi/oop/vasilchenko11/Tests/Demo.java
@@ -0,0 +1,13 @@
+package ua.khpi.oop.vasilchenko11.Tests;
+
+
+import java.io.IOException;
+import java.util.InputMismatchException;
+
+
+public class Demo {
+    public static void main(String[] args) throws InputMismatchException, IOException {
+        Lab11.main(new String[] {"-auto"});
+        Lab11.main(null);
+    }
+}
